665f435e8c6cd95cab9377dd906b65fd7e72cb94
IbrahimELE <118020507+IbrahimELE@users.noreply.github.com>
2024-10-26 11:27:55 +0200

    login, chat implement

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import FastAPI, Depends, HTTPException, status, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from typing import List
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 from database import get_db  
@@ -11,8 +13,7 @@ from schemas.likes import Likes
 from models.comments import CommentsDB
 from models.likes import LikesDB
 from crud.users import create_user, get_user_by_email, get_user_by_username
-from utils.authentication import create_access_token, verify_password, get_current_user, get_password_hash, create_refresh_token, store_refresh_token, store_access_token
-from utils.authentication import oauth2_scheme
+from utils.authentication import  verify_password, get_password_hash
 from fastapi.middleware.cors import CORSMiddleware
 import random
 from typing import Set
@@ -21,6 +22,78 @@ load_dotenv()
 
 app = FastAPI()
 
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Websocket Demo</title>
+           <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    </head>
+    <body>
+    <div class="container mt-3">
+        <h1>FastAPI WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="joinChat(event)">
+        <input type="text" class="form-control" id="usernameText" placeholder="Enter your username" required autocomplete="off"/>
+        <\button class="btn btn-outline-primary mt-2">Join</button>
+        </form>
+        <ul id='messages' class="mt-5">
+        </ul>
+        
+    </div>
+    
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+            function joinChat(event) {
+            var username = document.getElementById("usernameText").value;
+            client_id = username; // Use username as client_id
+            document.querySelector("#ws-id").textContent = client_id;
+            ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            event.preventDefault();
+            ws.onmessage = function(event) {
+            var messages = document.getElementById('messages');
+            var message = document.createElement('li');
+            var timestamp = new Date().toLocaleTimeString(); // Get the current time
+            var content = document.createTextNode(`[${timestamp}] ${event.data}`);
+            message.appendChild(content);
+            messages.appendChild(message);
+            var typingTimer; // Timer identifier
+            var doneTypingInterval = 1000; // Time in ms (1 second)
+
+            // User is typing
+            input.addEventListener("keyup", function() {
+                clearTimeout(typingTimer);
+                ws.send(client_id + " is typing..."); // Send typing message
+                typingTimer = setTimeout(doneTyping, doneTypingInterval);
+            });
+
+            // User is done typing
+            function doneTyping() {
+                ws.send(client_id + " stopped typing...");
+            }
+            };
+            }
+        </script>
+    </body>
+</html>
+"""
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["192.168.122.247:5173", "http://localhost:5173"],  
@@ -55,9 +128,7 @@ def register(user: UsersCreate, db: Session = Depends(get_db)):
     )
     create_user(db, user_data)
 
-    access_token = create_access_token(data={"sub": user.username})
-    
-    return {"user": user_data, "access_token": access_token, "token-type": "bearer", "message": "success"}
+    return {"user": user_data}
 
 @app.post("/login")
 async def login(user: LoginForm,  db: Session = Depends(get_db)):
@@ -72,39 +143,46 @@ async def login(user: LoginForm,  db: Session = Depends(get_db)):
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    access_token = create_access_token(data={"sub": userdb.username})
-    refresh_token = create_refresh_token(data={"sub": userdb.username})
 
-    store_access_token(db, userdb.id, access_token)
-    store_refresh_token(db, userdb.id, refresh_token)
-
-    return {
-        "access_token": access_token,
-        "token_type": "bearer"
-    }
+    return {userdb.username, userdb.first_name, userdb.last_name}
 
 
-@app.post("/posts")
-def create_post(post: PostsCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
-    db_post = PostsCreate(title=post.title, content=post.content, user_id=current_user.id)
-    db.add(db_post)
-    db.commit()
-    db.refresh(db_post)
-    return db_post
 
-blacklisted_tokens: Set[str] = set()  # Store blacklisted tokens in memory for simplicity
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+    
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
 
-@app.post("/logout")
-def logout(token: str = Depends(oauth2_scheme)):
-    blacklisted_tokens.add(token)
-    return {"message": "Logged out successfully"}
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
 
-def is_token_blacklisted(token: str) -> bool:
-    return token in blacklisted_tokens
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+    
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
 
-@app.delete("/delete/my_profile", status_code=status.HTTP_204_NO_CONTENT)
-def delete_current_user( db: Session = Depends(get_db), current_user: UsersDB = Depends(get_current_user)
-):
-    db.delete(current_user)
-    db.commit()
-    return {"detail": "User deleted successfully"}
+    
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws/{username}")
+async def websocket_endpoint(websocket: WebSocket, username: str):
+    await manager.connect(websocket)
+    try: 
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"{username}: {data}", websocket)
+            await manager.broadcast(f"{username} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"{username} has left the chat")
--- app/utils/authentication.py
+++ app/utils/authentication.py
@@ -9,29 +9,15 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from models.users import UsersDB
-from crud.users import get_user_by_email, get_user_by_id
+from crud.users import get_user_by_email
 from database import get_db
 
 load_dotenv()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
-def store_refresh_token(db: Session, user_id: int, refresh_token: str):
-    user = db.query(UsersDB).filter(UsersDB.id == user_id).first()
-    user.refresh_token = refresh_token  
-    db.commit()
-
-def store_access_token(db: Session, user_id: int, access_token: str):
-    user = db.query(UsersDB).filter(UsersDB.id == user_id).first()
-    
-    if user:
-        user.access_token = access_token
-        db.commit() 
-    else:
-        raise Exception("User not found")
-    
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
@@ -42,34 +28,6 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def validate_access_token(token: str):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        
-        if "exp" in payload and datetime.fromtimestamp(payload["exp"]) < datetime.now(timezone.utc):
-            raise JWTError("Token has expired")
-
-        return payload  
-
-    except JWTError:
-        raise Exception("Could not validate credentials") 
-    
-def create_refresh_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(days=30)  
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-def validate_refresh_token(refresh_token: str):
-    try:
-        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload  
-    except JWTError:
-        return None
-    
 def decode_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -85,8 +43,8 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
-"""
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):""
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -100,34 +58,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
     except JWTError:
         raise credentials_exception
 
-
     user = get_user_by_email(db, email=email)
     if user is None:
         raise credentials_exception
     return user
-    """
-
-
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> UsersDB:
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid token",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-        user = db.query(UsersDB).filter(UsersDB.username == username).first()
-        if user is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="User not found",
-            )
-        return user
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
